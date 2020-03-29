3996cb5e8e132c2dd321d29589cc24b74fd18d8f
dmartinchick <dmartinchick@gmail.com>
2020-03-29 15:58:47 +0300

    Разметка Html
    Сделано:
    - создана разметка блоков table, events,history и footer
    - сделана прокрутка по якорям

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    //прокурутка по якорям
+    $(".navbar-menu").on("click","a", function(e){
+        e.preventDefault();
+        var id=$(this).attr('href'),
+            top=$(id).offset().top;
+        $('body, html').animate({scrollTop:top-60},500);
+    });
+});
