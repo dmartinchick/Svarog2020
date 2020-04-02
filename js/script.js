8018ee1c269869d661a01e3a027f83cdace1be18
dmartinchick <dmartinchick@gmail.com>
2020-04-02 22:39:07 +0300

    правки
    сделано:
    - Hover эффект в modal-menu
    - after для modal-logo

--- js/script.js
+++ js/script.js
@@ -7,6 +7,12 @@ $(document).ready(function () {
             top=$(id).offset().top;
         $('body, html').animate({scrollTop:top-60},500);
     });
+    $(".modal-menu").on("click","a", function(e){
+        e.preventDefault();
+        var id=$(this).attr('href'),
+            top=$(id).offset().top;
+		$('body, html').animate({scrollTop:top-60},500);
+	});
 
     //скролл флага
     $(window).scroll(function(){
